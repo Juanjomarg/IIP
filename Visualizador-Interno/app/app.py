08f8ffd03cf993041829403501cc93114c4c4997
juanjomarg <juanj.martinezg96@gmail.com>
2024-04-08 17:43:02 -0500

    multiple fixes

--- Visualizador-Interno/app/app.py
+++ Visualizador-Interno/app/app.py
@@ -120,4 +120,4 @@ def sel_entidad(value):
         return ent
     
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
